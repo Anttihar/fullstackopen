04903ea93c2fff13ef1391aeb857c39641f1f184
Anttihar <antti.harra@gmail.com>
2023-11-13 15:13:13 +0200

    step2

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -18,14 +18,49 @@ const Stats = ({ stats, text }) => {
   )
 }
 
+const Average = ({ average, total, text }) => {
+  if (total === 0) {
+    return (
+    <p>{text}0</p>
+    )
+  }
+  return (
+     <p>{text}{average / total}</p>
+  )
+}
+
+const Prosent = ({ good, total, text }) => {
+  if (total === 0) {
+    return (
+      <p>{text}0</p>
+    )
+  }
+  return (
+    <p>{text}{good / total * 100}%</p>
+  )
+}
+
 const App = () => {
   const [ good, setGood ] = useState(0)
   const [ neutral, setNeutral ] = useState(0)
   const [ bad, setBad ] = useState(0)
+  const [ total, setTotal ] = useState(0)
+  const [ average, setAverage ] = useState(0)
 
-  const goodClick = () => setGood(good + 1)
-  const neutraClick = () => setNeutral(neutral + 1)
-  const badClick = () => setBad(bad + 1)
+  const goodClick = () => {
+    setGood(good + 1)
+    setTotal(total + 1)
+    setAverage(average + 1 )
+  }
+  const neutraClick = () => {
+    setNeutral(neutral + 1)
+    setTotal(total + 1)
+  }
+  const badClick = () => {
+    setBad(bad + 1)
+    setTotal(total + 1)
+    setAverage(average - 1)
+  }
 
   return (
     <div>
@@ -37,6 +72,9 @@ const App = () => {
       <Stats stats={good} text="Hyvä: " />
       <Stats stats={neutral} text="Neutraali: " />
       <Stats stats={bad} text="Huono: " />
+      <Stats stats={total} text="Yhteensä: " />
+      <Average average={average} total={total} text="Keskiarvo: " />
+      <Prosent good={good} total={total} text="Positiivisia: " />
     </div>
   )
 }
