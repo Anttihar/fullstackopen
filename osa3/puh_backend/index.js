278b9d03980284538f663e56dbb527c8cb6beefc
Anttihar <antti.harra@gmail.com>
2023-12-07 10:39:42 +0200

    step2

--- osa3/puh_backend/index.js
+++ osa3/puh_backend/index.js
@@ -23,6 +23,13 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+app.get('/info', (req, res) => {
+    res.send(
+        `<p>Phonebook has info for ${persons.length} people</p>
+        <p>${Date()}</p>`
+    )
+})
+
 
 const PORT = 3001
 app.listen(PORT, () => {
