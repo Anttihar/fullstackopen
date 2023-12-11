2e72d8d04ceaebbe67fc37e64e0bab87528f1a09
Anttihar <antti.harra@gmail.com>
2023-12-11 14:40:37 +0200

    mongo db

--- harjoituksia/node/index.js
+++ harjoituksia/node/index.js
@@ -1,35 +1,17 @@
+require('dotenv').config()
 const express = require('express')
+const app = express()
+const Note = require('./models/note')
 const cors = require('cors')
 
-const app = express()
 app.use(express.json())
 app.use(cors())
 app.use(express.static('dist'))
 
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only JavaScript",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true
-    }
-]
-
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
-})
-
 app.get('/api/notes', (req, res) => {
-    res.json(notes)
+    Note.find({}).then(notes => {
+        res.json(notes)
+    })
 })
 
 app.get('/api/notes/:id', (req, res) => {
@@ -75,6 +57,6 @@ app.post('/api/notes', (req, res) => {
     res.json(note)
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
