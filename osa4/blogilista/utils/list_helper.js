cae5fc4a65f5986671d71d76e8105f0b0704b9c5
Anttihar <antti.harra@gmail.com>
2023-12-19 00:27:59 +0200

    apufunktioita ja yksikkötestejä step 3

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -9,8 +9,15 @@ const totalLikes = blogs => {
     : blogs.reduce((sum, blog) => sum + blog.likes, 0)
     )
 }
+
+const favoriteBlogs = blogs => {
+    const likes = blogs.map(blog => blog.likes)
+    const maxLikes = likes.indexOf(Math.max(...likes))
+    return blogs[maxLikes]
+}
   
   module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlogs
   }
