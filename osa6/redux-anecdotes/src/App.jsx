d3102253729749e6ac8f0e2b1688fea97a8078ad
Anttihar <antti.harra@gmail.com>
2024-04-21 18:52:10 +0300

    step7

--- osa6/redux-anecdotes/src/App.jsx
+++ osa6/redux-anecdotes/src/App.jsx
@@ -1,10 +1,13 @@
 import Anecdotes from "./components/AnecdoteList"
 import AddAnecdote from "./components/AnecdoteForm"
+import Filter from "./components/Filter"
 
 const App = () => {
 
   return (
     <div>
+      <h2>Anecdotes</h2>
+      <Filter />
       <Anecdotes />
       <AddAnecdote />
     </div>
--- osa6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ osa6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -3,24 +3,29 @@ import { vote } from '../reducers/anecdoteReducer'
 
 const Anecdotes = () =>{
   const dispatch = useDispatch()
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(({ filter, anecdotes }) => {
+    if (filter) {
+    return anecdotes.filter(anecdote => 
+      anecdote.content.toLowerCase().includes(filter.toLowerCase()))
+    }
+    return anecdotes
+  })
 
   const sortedAnecdotes = anecdotes.sort((a, b) => b.votes - a.votes)
 
   return(
     <div>
-      <h2>Anecdotes</h2>
-        {sortedAnecdotes.map(anecdote =>
-          <ul key={anecdote.id}>
-            <div>
-              {anecdote.content}
-            </div>
-            <div>
-              has {anecdote.votes}
-              <button onClick={ () => dispatch(vote(anecdote.id))}>vote</button>
-            </div>
-          </ul>
-        )}
+      {sortedAnecdotes.map(anecdote =>
+        <ul key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={ () => dispatch(vote(anecdote.id))}>vote</button>
+          </div>
+        </ul>
+      )}
     </div>
   )
 }
